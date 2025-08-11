e2d8d995423e2bb712e1102f43484f025c807f17
JOSE DOUGLAS SILVA DE CARVALHO <douglassilva11@hotmail.com>
2025-08-11 11:44:47 -0300

    Ecommerce.py

--- Ecommerce.py
+++ Ecommerce.py
@@ -1,1 +1,23 @@
+import pytest
+from src.validar import validar_voucher
 
+def test_valor_minimo():
+    assert validar_voucher(50.00) is True
+
+def test_valor_maximo():
+    assert validar_voucher(500.00) is True
+
+def test_valor_abaixo_do_minimo():
+    assert validar_voucher(49.99) is False
+
+def test_valor_acima_do_maximo():
+    assert validar_voucher(500.01) is False
+
+def test_valor_intermediario():
+    assert validar_voucher(100.00) is True
+
+def test_valor_muito_abaixo():
+    assert validar_voucher(0.00) is False
+
+def test_valor_muito_acima():
+    assert validar_voucher(1000.00) is False
