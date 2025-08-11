e4a2e3c2b414787fae82c4fba6834f49640e49eb
JOSE DOUGLAS SILVA DE CARVALHO <douglassilva11@hotmail.com>
2025-08-11 11:52:30 -0300

    Classificador.py

--- Classificador.py
+++ Classificador.py
@@ -1,1 +1,11 @@
+# arquivo: classificador.py
 
+def classificar_nota(nota):
+    """Classifica um Valor numérico de 50 a 500."""
+    if nota > 500 or nota < 0:
+        return "Nota inválida"
+
+    if nota >= 50:
+        return "Aprovado"
+    else:
+        return "Reprovado"
